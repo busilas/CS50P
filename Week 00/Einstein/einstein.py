bf1f913c0084624503207ee5db33c961e8901908
Andrius Busilas <andrius.busilas@gmail.com>
2024-03-18 22:55:39 +0200

    Add files via upload

---
+++ Week 00/Einstein/einstein.py
@@ -0,0 +1,11 @@
+# Prompt mass
+mass = int(input("m: "))
+
+# Speed of light
+speed_of_light = 300000000
+
+# Calculating energy using Einstein's famous formula
+joules = mass * speed_of_light ** 2
+
+# Outputting the equivalent energy in Joules
+print("E:", joules)
